3f52f89348bdb11a9eda9bbaa31c2833a7f1472d
t-xiaodan <1609910273@qq.com>
2021-09-22 11:23:31 +0800

    修改banner跳转链接

--- docs/.vuepress/components/IndexHome.vue
+++ docs/.vuepress/components/IndexHome.vue
@@ -9,9 +9,9 @@
                         随时随地享受私有云
                     </div>
                     <div class="words">易有云提供全平台的数据互通，带你享受P2P的极速体验。随时随地备份你的照片、文件，轻松打造属于你的私有云服务。</div>
-                    <router-link to="/zh/docs/">
+                    <a href="https://doc.linkease.com/zh/guide/linkease_app/" target="_blank">
                         <div class="btn">立即体验</div>
-                    </router-link>
+                    </a>
 
                 </div>
             </div>
